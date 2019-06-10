2e2e101490a68c5ccd0a91da60013d5588145fa7
MarkMaTeng <markmateng@gmail.com>
2019-06-10 01:56:35 -0700

    2.18

--- Project/js/prefabs/level1/uselessClosetEvent.js
+++ Project/js/prefabs/level1/uselessClosetEvent.js
@@ -1,5 +1,5 @@
 function openSinkCloset(){
-	if(inOven == false && inBanana == false && inPipe == false && inPhone == false && inStair = false){
+	if(inOven == false && inBanana == false && inPipe == false && inPhone == false && inStair == false){
 		this.sinkCloset.destroy();
 		this.openedPipeCloset = game.add.button(275, 185, 'sinkcloset', closeSinkCloset, this, 0, 0, 0);
 		this.openCloset.play();
@@ -8,7 +8,7 @@ function openSinkCloset(){
 }
 
 function closeSinkCloset(){
-	if(inOven == false && inBanana == false && inPipe == false && inPhone == false && inStair = false){
+	if(inOven == false && inBanana == false && inPipe == false && inPhone == false && inStair == false){
 		this.openedPipeCloset.destroy();
 		this.closeCloset.play();
 		this.sinkCloset = game.add.button(350, 200, 'trigger', openSinkCloset, this, 0, 0, 0);
@@ -18,7 +18,7 @@ function closeSinkCloset(){
 }
 
 function openUpCloset(){
-	if(inOven == false && inBanana == false && inPipe == false && inPhone == false && inStair = false){
+	if(inOven == false && inBanana == false && inPipe == false && inPhone == false && inStair == false){
 		this.upCloset.destroy();
 		this.openedUpCloset = game.add.button(550, -13, 'upcloset', closeUpCloset, this, 0, 0, 0);
 		this.openCloset.play();
@@ -26,7 +26,7 @@ function openUpCloset(){
 }
 
 function closeUpCloset(){
-	if(inOven == false && inBanana == false && inPipe == false && inPhone == false && inStair = false){
+	if(inOven == false && inBanana == false && inPipe == false && inPhone == false && inStair == false){
 		this.openedUpCloset.destroy();
 		this.closeCloset.play();
 		this.upCloset = game.add.button(600, 0, 'trigger', openUpCloset, this, 0, 0, 0);
